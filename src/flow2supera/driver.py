8bca55897b384caf2e33314cb16192de9e753993
sindhu-ku <s.kumaran@uci.edu>
2024-03-14 01:49:47 -0700

    rm spaces

--- src/flow2supera/driver.py
+++ src/flow2supera/driver.py
@@ -237,7 +237,6 @@ class SuperaDriver(edep2supera.edep2supera.SuperaDriver):
         p.end_pz = trajectory['pxyz_end'][2]
         p.energy_init = trajectory['E_start']
         p.dist_travel = trajectory['dist_travel']
-
         #This is equivalent to np.sqrt(pow(flow2supera.pdg2mass.pdg2mass(p.pdg),2) + 
         #                        pow(p.px,2) + pow(p.py,2) + pow(p.pz,2))
         # TODO Is this correct? Shouldn't the vertex be the interaction vertex?
