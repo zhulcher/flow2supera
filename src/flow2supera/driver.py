1f017b92423e511f025897d2793efa3bf2dba232
sindhu-ku <s.kumaran@uci.edu>
2024-05-08 15:50:47 -0700

    revert genid change

--- src/flow2supera/driver.py
+++ src/flow2supera/driver.py
@@ -242,8 +242,9 @@ class SuperaDriver(edep2supera.edep2supera.SuperaDriver):
         # but Supera/LArCV want a regular int, hence the type casting
         p.id             = int(trajectory['event_id'])
         p.interaction_id = int(trajectory['vertex_id'])
-        p.trackid        = int(trajectory['file_traj_id']) # Unique among all files used in SuperaAtomic
-        p.genid          = int(trajectory['traj_id'])
+        p.trackid        = int(trajectory['file_traj_id']) # Unique among all files used in truth-matching for MLreco
+        if hasattr(p, "genid") and trajectory["parent_id"] < 0:# < 0 indicates a top-level particle (from GENIE)
+            p.genid = int(trajectory['traj_id'])
         p.pdg            = int(trajectory['pdg_id'])
         p.px = trajectory['pxyz_start'][0] 
         p.py = trajectory['pxyz_start'][1] 
